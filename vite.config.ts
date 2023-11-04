8ecec435f37dd02c4949e57fc44a5ddc3d23ec51
Daouadji Aymen <daouadji506@gmail.com>
2023-11-04 21:47:25 +0100

    🚑 add Vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,61 @@
 import { rmSync } from 'node:fs'
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import pkg from './package.json'
+import svgr from 'vite-plugin-svgr';
+import dotenv from 'dotenv';
+
+const env = dotenv.config().parsed;
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command,mode }) => {
   rmSync('dist-electron', { recursive: true, force: true })
 
   const isServe = command === 'serve'
   const isBuild = command === 'build'
   const sourcemap = isServe || !!process.env.VSCODE_DEBUG
 
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
   return {
+    define: {
+      'process.env.API_BASE_URL': JSON.stringify(process.env.API_BASE_URL),
+    },
     resolve: {
       alias: {
-        '@': path.join(__dirname, 'src')
+        // Define your module aliases here
+        '@components': path.resolve(__dirname, 'src/ui/components'),
+        '@buttons': path.resolve(__dirname, 'src/ui/components/buttons'),
+        '@containers': path.resolve(__dirname, 'src/ui/containers'),
+        '@layers': path.resolve(__dirname, 'src/ui/layers'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@libs': path.resolve(__dirname, 'src/libs'),
+        '@constants': path.resolve(__dirname, 'src/utils/constants'),
+        '@helpers': path.resolve(__dirname, 'src/utils/helpers'),
+        '@models': path.resolve(__dirname, 'src/models'),
+        toSvg: path.resolve(__dirname, 'src/assets/svg'),
+        toPng: path.resolve(__dirname, 'src/assets/pictures'),
+        '@api': path.resolve(__dirname, 'src/API'),
+        '@colors': path.resolve(__dirname, 'src/assets/styles/color.ts'),
+        '@stores': path.resolve(__dirname, 'src/services/zustand'),
+        '@services': path.resolve(__dirname, 'src/services'),
+        '~styles': path.resolve(__dirname, 'src/assets/styles/'),
+        '~font': path.resolve(__dirname, 'src/assets/font/'),
       },
     },
     plugins: [
-      react(),
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+      svgr({
+  
+      }),
       electron([
         {
           // Main-Process entry file of the Electron App.
