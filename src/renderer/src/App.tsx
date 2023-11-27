ef409a96e7fcbbf1aa2119c0416a21c9abe91549
Daouadji Aymen <daouadji506@gmail.com>
2023-11-27 02:06:19 +0100

    ✨ use Main Layer

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.scss';
+import MainLayer from '@containers/layers/main_layer';
 
 console.log(' window.prisma', window.prisma);
 
@@ -7,33 +8,9 @@ const user = await window.prisma.getUsers();
 console.log('user', user);
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="app-container">
-      <div className="logo-box">
-        <a
-          href="https://github.com/electron-vite/electron-vite-react"
-          target="_blank"
-          rel="noreferrer"
-        ></a>
-      </div>
-      <h1>Electron + Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Electron + Vite logo to learn more
-      </p>
-      <div className="flex-center">
-        Place static files into the<code>/public</code> folder{' '}
-      </div>
+      <MainLayer />
     </div>
   );
 }
