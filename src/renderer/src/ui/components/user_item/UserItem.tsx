e04ce4701f88bdd033d6d9a7df3f52604be82769
Daouadji Aymen <daouadji506@gmail.com>
2023-11-29 15:11:44 +0100

    ✅ Update UserItem comp

--- src/renderer/src/ui/components/user_item/UserItem.tsx
+++ src/renderer/src/ui/components/user_item/UserItem.tsx
@@ -1,5 +1,15 @@
 import './style/index.scss';
+import user_image from 'toPng/user_profile_test.png';
+
 interface UserItemProps {}
 export default function UserItem({}: UserItemProps) {
-  return <div className="user-item"></div>;
+  return (
+    <div className="user-item">
+      <img src={user_image} alt="user" width={100} height={100} />
+      <div className="user-info-container">
+        <span>Ghanou faycel</span>
+        <span>Owner</span>
+      </div>
+    </div>
+  );
 }
