808469cb46a85969eb845b05b10ece7ce3a0dfdf
Daouadji Aymen <daouadji506@gmail.com>
2023-12-07 00:35:24 +0100

    ✨ Render dynamic routes

--- src/renderer/src/ui/containers/layers/main_layer/pages/app_menu/AppMenu.tsx
+++ src/renderer/src/ui/containers/layers/main_layer/pages/app_menu/AppMenu.tsx
@@ -1,14 +1,10 @@
 import MenuOption from '@components/menu_option';
 import './style/index.scss';
 import Logo from 'toSvg/logo.svg';
-import { BiSolidDashboard } from 'react-icons/bi';
 import { RiShoppingBag3Fill } from 'react-icons/ri';
-import { IoDocumentTextSharp } from 'react-icons/io5';
-import { MdGroups } from 'react-icons/md';
-import { FaCreditCard } from 'react-icons/fa';
-import { IoMdSettings } from 'react-icons/io';
-import TextButton from '@components/buttons/text_button';
-import color from '@assets/styles/color';
+import MenuDropdown from '@components/menu_dropdown';
+import { routesData } from '@services/routes';
+
 
 interface AppMenuProps {}
 export default function AppMenu({}: AppMenuProps) {
@@ -16,50 +12,20 @@ export default function AppMenu({}: AppMenuProps) {
     <div className="app-menu">
       <Logo width={180} height={50} />
       <div className="menu-container">
-        <MenuOption
-          name="Overview"
-          Icon={BiSolidDashboard}
-          selected={true}
-          onClick={() => {
-            console.log('overview');
-          }}
-        />
+        {routesData.map((route, index) => (
+          <MenuDropdown
+            key={index}
+            mainRouteName={route.mainRouteName}
+            mainRouteIcon={route.mainRouteIcon}
+            routes={route.routes}
+          />
+        ))}
+
         <MenuOption
           name="Products"
           Icon={RiShoppingBag3Fill}
           onClick={() => {
             console.log('products');
-          }}
-        />
-        <MenuOption
-          name="Orders"
-          Icon={IoDocumentTextSharp}
-          onClick={() => {
-            console.log('orders');
-          }}
-        />
-        <MenuOption
-          name="Customers"
-          Icon={MdGroups}
-          iconProps={{
-            fontSize: 25,
-          }}
-          onClick={() => {
-            console.log('customers');
-          }}
-        />
-        <MenuOption
-          name="Payments"
-          Icon={FaCreditCard}
-          onClick={() => {
-            console.log('payments');
-          }}
-        />
-        <MenuOption
-          name="Settings"
-          Icon={IoMdSettings}
-          onClick={() => {
-            console.log('settings');
           }}
         />
       </div>
