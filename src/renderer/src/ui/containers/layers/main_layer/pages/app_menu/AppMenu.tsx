f9888277c14edab1ebe79d81fe4c4d751cb7379d
Daouadji Aymen <daouadji506@gmail.com>
2023-12-07 23:45:57 +0100

    🐛 Fix route detection

--- src/renderer/src/ui/containers/layers/main_layer/pages/app_menu/AppMenu.tsx
+++ src/renderer/src/ui/containers/layers/main_layer/pages/app_menu/AppMenu.tsx
@@ -1,14 +1,19 @@
-import MenuOption from '@components/menu_option';
 import './style/index.scss';
 import Logo from 'toSvg/logo.svg';
-import { AnimatePresence, motion, LayoutGroup } from 'framer-motion';
 import MenuDropdown from '@components/menu_dropdown';
 import { routesData } from '@services/routes';
 import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import MenuOption from '@components/menu_option';
+import { FaCircle } from 'react-icons/fa';
 
 interface AppMenuProps {}
 export default function AppMenu({}: AppMenuProps) {
-  const [isOpen, setisOpen] = useState();
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const location = useLocation();
+  const pathname = location.pathname.split('/');
+  const navigate = useNavigate();
   return (
     <div className="app-menu">
       <Logo width={180} height={50} />
@@ -20,7 +25,56 @@ export default function AppMenu({}: AppMenuProps) {
               mainRouteName={route.mainRouteName}
               mainRouteIcon={route.mainRouteIcon}
               routes={route.routes}
-            />
+              setIsListShown={() => setSelectedIndex(index)}
+              selected={index === selectedIndex}
+              mainRouteSelected={
+                location.pathname === '/' && index === 0
+                  ? true
+                  : routesData.some((rd) => {
+                      if (
+                        route.routes &&
+                        route.mainRouteName == rd.mainRouteName
+                      ) {
+                        return route.routes.some((r) => {
+                          return r.path === pathname[pathname.length - 1];
+                        });
+                      }
+                    })
+              }
+            >
+              {route.routes?.map((route, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{
+                    duration: 0.3,
+                    ease: 'easeInOut',
+                    delay: index * 0.1,
+                  }}
+                >
+                  <MenuOption
+                    key={index}
+                    name={route.name}
+                    Icon={FaCircle}
+                    selected={pathname[pathname.length - 1] === route.path}
+                    onClick={() => {
+                      navigate(route.path);
+                    }}
+                    iconProps={{
+                      size: 12,
+                      opacity:
+                        pathname[pathname.length - 1] === route.path ? 1 : 0.2,
+                    }}
+                    buttonProps={{
+                      alignSelf: 'flex-end',
+                      width: 250,
+                    }}
+                  />
+                </motion.div>
+              ))}
+            </MenuDropdown>
           </div>
         ))}
       </div>
