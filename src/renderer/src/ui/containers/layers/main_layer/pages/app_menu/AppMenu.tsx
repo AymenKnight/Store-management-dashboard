ecfab4ca75c627d54c140babc23955a02c535e3d
Daouadji Aymen <daouadji506@gmail.com>
2023-12-07 20:51:05 +0100

    ✅ Update AppMenu comp

--- src/renderer/src/ui/containers/layers/main_layer/pages/app_menu/AppMenu.tsx
+++ src/renderer/src/ui/containers/layers/main_layer/pages/app_menu/AppMenu.tsx
@@ -1,33 +1,28 @@
 import MenuOption from '@components/menu_option';
 import './style/index.scss';
 import Logo from 'toSvg/logo.svg';
-import { RiShoppingBag3Fill } from 'react-icons/ri';
+import { AnimatePresence, motion, LayoutGroup } from 'framer-motion';
 import MenuDropdown from '@components/menu_dropdown';
 import { routesData } from '@services/routes';
-
+import { useState } from 'react';
 
 interface AppMenuProps {}
 export default function AppMenu({}: AppMenuProps) {
+  const [isOpen, setisOpen] = useState();
   return (
     <div className="app-menu">
       <Logo width={180} height={50} />
       <div className="menu-container">
         {routesData.map((route, index) => (
-          <MenuDropdown
-            key={index}
-            mainRouteName={route.mainRouteName}
-            mainRouteIcon={route.mainRouteIcon}
-            routes={route.routes}
-          />
+          <div key={route.mainRouteName}>
+            <MenuDropdown
+              key={route.mainRouteName}
+              mainRouteName={route.mainRouteName}
+              mainRouteIcon={route.mainRouteIcon}
+              routes={route.routes}
+            />
+          </div>
         ))}
-
-        <MenuOption
-          name="Products"
-          Icon={RiShoppingBag3Fill}
-          onClick={() => {
-            console.log('products');
-          }}
-        />
       </div>
     </div>
   );
