6661b1dab8172923be3922713d521d2262d2f593
Daouadji Aymen <daouadji506@gmail.com>
2023-12-07 20:50:14 +0100

    ✨ add animation on MenuDropdown

--- src/renderer/src/ui/components/menu_dropdown/MenuDropdown.tsx
+++ src/renderer/src/ui/components/menu_dropdown/MenuDropdown.tsx
@@ -4,6 +4,7 @@ import { FaCircle } from 'react-icons/fa';
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { routesData } from '@services/routes';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface MenuDropdownProps {
   mainRouteName: string;
@@ -23,20 +24,28 @@ export default function MenuDropdown({
   routes,
 }: MenuDropdownProps) {
   const [isListShown, setIsListShown] = useState(false);
+
   const location = useLocation();
   const navigate = useNavigate();
+
   const pathname = location.pathname.split('/');
 
   console.log('pathname', pathname[pathname.length - 1]);
   return (
-    <div className="menu-dropdown">
+    <motion.div
+      className="menu-dropdown"
+      initial={{ gap: 0 }}
+      animate={{ gap: 10 }}
+      exit={{ gap: 0 }}
+      transition={{ duration: 0.3, ease: 'easeInOut' }}
+    >
       <MenuOption
         name={mainRouteName}
         Icon={mainRouteIcon}
         selected={
           routes
             ? routesData.some((route) => {
-                if (route.routes)
+                if (route.routes && route.mainRouteName == mainRouteName)
                   return route.routes.some((route) => {
                     return route.path === pathname[pathname.length - 1];
                   });
@@ -47,28 +56,50 @@ export default function MenuDropdown({
           setIsListShown(!isListShown);
         }}
       />
-      {isListShown && routes && routes.length > 0 && (
-        <div className="menu-dropdown-list">
-          {routes.map((route, index) => (
-            <MenuOption
-              key={index}
-              name={route.name}
-              Icon={FaCircle}
-              selected={pathname[pathname.length - 1] === route.path}
-              onClick={() => {
-                navigate(route.path);
-              }}
-              iconProps={{
-                size: 12,
-              }}
-              buttonProps={{
-                alignSelf: 'flex-end',
-                width: 250,
-              }}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+      <AnimatePresence mode="wait">
+        {isListShown && routes && routes.length > 0 && (
+          <motion.div
+            className="menu-dropdown-list"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: 'auto' }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}
+          >
+            {routes.map((route, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{
+                  duration: 0.3,
+                  ease: 'easeInOut',
+                  delay: index * 0.1,
+                }}
+              >
+                <MenuOption
+                  key={index}
+                  name={route.name}
+                  Icon={FaCircle}
+                  selected={pathname[pathname.length - 1] === route.path}
+                  onClick={() => {
+                    navigate(route.path);
+                  }}
+                  iconProps={{
+                    size: 12,
+                    opacity:
+                      pathname[pathname.length - 1] === route.path ? 1 : 0.2,
+                  }}
+                  buttonProps={{
+                    alignSelf: 'flex-end',
+                    width: 250,
+                  }}
+                />
+              </motion.div>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
   );
 }
